d6435e09480830986fc72d253afe1666acffa4df
jack_glb <rose@163.com>
2019-01-19 11:51:04 +0800

    It's the first time to commit on eclipse

---
+++ src/main/java/cn/itcast/core/web/LoginAction.java
@@ -0,0 +1,8 @@
+package cn.itcast.core.web;
+
+public class LoginAction {
+
+	public static void main(String[] args) {
+		System.out.println("i start to login in");
+	}
+}
---
+++ src/test/java/cn/itcast/core/test/LoginActionTest.java
@@ -0,0 +1,12 @@
+package cn.itcast.core.test;
+
+import org.junit.Test;
+
+public class LoginActionTest {
+
+	@Test
+	public void test() {
+		System.out.println("i start to test");
+	}
+
+}
