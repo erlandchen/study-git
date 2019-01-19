914d1b7dd840620a494160b85c79c8aba4440de9
jack_glb <rose@163.com>
2019-01-19 19:12:37 +0800

    solve conflict

--- src/main/java/cn/itcast/core/web/LoginAction.java
+++ src/main/java/cn/itcast/core/web/LoginAction.java
@@ -3,7 +3,7 @@ package cn.itcast.core.web;
 public class LoginAction {
 
 	public static void main(String[] args) {
-		System.out.println("i start to login in right");
 		System.out.println("i start to login in left");
+		System.out.println("i start to login in right");
 	}
 }
